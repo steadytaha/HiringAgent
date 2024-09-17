91661f20ec94609eef6e8a4a89e007a033dc460c
Taha Efe <littleman.teg@gmail.com>
2024-09-17 10:23:07 +0300

    Test brach for pdfs.

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from st_files_connection import FilesConnection
 
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket-10/Blog.csv", input_format="csv", ttl=600)
+documents = conn.read("streamlit-bucket-10/Test/", input_format="pdf", ttl=600)
 
-for row in df.itertuples():
-    st.write(f"{row.Title} is in :{row.Category}: genre.")
+st.write(documents)
+
+"""for row in df.itertuples():
+    st.write(f"{row.Title} is in {row.Category} genre.")"""
